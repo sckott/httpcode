a27d2559e621cd6527d8bbffceb4051b8fa59383
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-30 06:54:56 -0800

    added browse param to pet fxns, default is not open image

--- R/cat_for_status.R
+++ R/cat_for_status.R
@@ -1,8 +1,12 @@
-#' Use cat pictures for various HTTP status codes
+#' Use cat or dog pictures for various HTTP status codes
 #'
 #' @export
 #' @name cat_for_status
+#'
 #' @param code An http status code
+#' @param browse (logical) If TRUE, opens image in default browser. If FALSE,
+#' returns the URL of the image.
+#' @return Opens image in your default browser, or returns URL
 #' @examples \donttest{
 #' cat_for_status(100)
 #' dog_for_status(100)
@@ -15,20 +19,20 @@
 #' # not found
 #' cat_for_status(555)
 #' }
-cat_for_status <- function(code){
+cat_for_status <- function(code, browse=FALSE){
   code <- as.character(code)
   if(code %in% names(status_codes)){
     url <- sprintf('http://httpcats.herokuapp.com/%s', code)
-    browseURL(url)
+    if(browse) browseURL(url) else url
   } else { stopcode("No cat code found", "") }
 }
 
 #' @export
 #' @rdname cat_for_status
-dog_for_status <- function(code){
+dog_for_status <- function(code, browse=FALSE){
   code <- as.character(code)
   if(code %in% names(status_codes)){
     url <- sprintf('http://httpstatusdogs.com/wp-content/uploads/%s.jpg', code)
-    browseURL(url)
+    if(browse) browseURL(url) else url
   } else { stopcode("No dog code found", "") }
 }
