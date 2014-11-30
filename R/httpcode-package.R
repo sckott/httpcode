2c14c97b751383285ceaae12a3abad28e2329934
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-29 22:01:49 -0800

    first working version

---
+++ R/httpcode-package.R
@@ -0,0 +1,9 @@
+#' Explain the meaning of HTTP status codes
+#'
+#' @name httpcode-package
+#' @aliases httpcode
+#' @docType package
+#' @title Explain the meaning of HTTP status codes
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
