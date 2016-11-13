83a5809d7cb4473223edd5780c3295bb9d2f3e21
Scott Chamberlain <myrmecocystus@gmail.com>
2016-11-13 15:01:05 -0800

    fix #3, added verbose http messages

--- R/code.R
+++ R/code.R
@@ -5,11 +5,17 @@
 #' status codes.
 #' @param text (character) A text string to search the messages or descriptions
 #' of HTTP status codes.
+#' @param verbose (logical) include verbose status code explanation.
+#' Default: \code{FALSE}
 #' @examples
 #' # search by code
 #' http_code(100)
 #' http_code(400)
 #' http_code(503)
+#' ## verbose explanation
+#' http_code(100, verbose = TRUE)
+#' http_code(400, verbose = TRUE)
+#' http_code(503, verbose = TRUE)
 #'
 #' # fuzzy code search
 #' http_code('1xx')
@@ -17,11 +23,16 @@
 #' http_code('30[12]')
 #' http_code('30[34]')
 #' http_code('30[34]')
+#' ## verbose explanation
+#' http_code('1xx', verbose = TRUE)
+#' http_code('3xx', verbose = TRUE)
 #'
 #' # search by text message
 #' http_search("request")
 #' http_search("forbidden")
 #' http_search("too")
+#' ## verbose explanation
+#' http_search("request", verbose = TRUE)
 #'
 #' @examples \dontrun{
 #' http_search("birds")
@@ -30,38 +41,48 @@
 
 #' @export
 #' @rdname http
-http_code <- function(code = NULL){
+http_code <- function(code = NULL, verbose = FALSE) {
   code <- as.character(code)
   if (is.null(code)) {
     print_codes()
   } else {
     if (is_three_digit_code(code)) {
-      print_code(code)
+      print_code(code, verbose)
     } else {
-      print_filtered_codes(code)
+      print_filtered_codes(code, verbose)
     }
   }
 }
 
 #' @export
 #' @rdname http
-http_search <- function(text = NULL) print_search(text)
+http_search <- function(text = NULL, verbose = FALSE) print_search(text, verbose)
 
 
 # helpers -------------------------------
-print_filtered_codes <- function(code){
+print_filtered_codes <- function(code, verbose){
   code2 <- paste0(gsub("x", "\\\\d", code), "$")
   found_codes <- nozero(sapply(names(status_codes), function(x) grep(code2, x, value = TRUE)))
   if (length(found_codes) == 0) stopcode('No code found corresponding to', code)
-  print_codes(found_codes)
+  print_codes(found_codes, verbose)
 }
 
-print_codes <- function(codes=names(status_codes)) lapply(sort(codes), print_code)
+print_codes <- function(codes = names(status_codes), verbose) {
+  lapply(sort(codes), print_code, verbose = verbose)
+}
 
-print_code <- function(code){
+print_code <- function(code, verbose) {
   twocodes <- status_codes[code]
-  if (length(twocodes[[1]]) != 2) stopcode('No description found for code', code)
-  structure(msg_list(code, twocodes[[1]][[1]], twocodes[[1]][[2]]), class = "http_code")
+  if (length(twocodes[[1]]) != 3) stopcode('No description found for code', code)
+  structure(
+    msg_list(
+      code,
+      twocodes[[1]][[1]],
+      twocodes[[1]][[2]],
+      if (verbose) twocodes[[1]][[3]] else NULL
+    ),
+    class = "http_code"
+  )
 }
 
 #' @export
@@ -69,15 +90,18 @@ print.http_code <- function(x, ...){
   cat(sprintf("<Status code: %s>", x$status_code), sep = "\n")
   cat(sprintf("  Message: %s", x$message), sep = "\n")
   cat(sprintf("  Explanation: %s", x$explanation), sep = "\n")
+  if (!is.null(x$verbose_explanation)) {
+    cat(sprintf("  Verbose Explanation: %s", x$verbose_explanation), sep = "\n")
+  }
 }
 
-print_search <- function(text){
+print_search <- function(text, verbose) {
   found_codes <- vapply(status_codes, function(x){
-    any(vapply(x, function(y) grepl(text, y, ignore.case = TRUE), logical(1)))
+    any(vapply(x, function(y) grepl(text, y %||% "", ignore.case = TRUE), logical(1)))
   }, logical(1))
   if (any(found_codes)) {
-    print_codes(names(status_codes[found_codes]))
+    print_codes(names(status_codes[found_codes]), verbose)
   } else {
-    stopcode('No status code found for search: ', text)
+    stopcode('No status code found for search', text)
   }
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,16 @@
 msg_format <-
   "<Status code: %s>
   Message: %s
-  Explanation: %s"
+  Explanation: %s,
+  Verbose Explanation: %s"
 
-msg_list <- function(a, b, c){
-  list(status_code = a,
-       message = b,
-       explanation = c
-  )
+msg_list <- function(a, b, c, d){
+  comp(list(
+    status_code = a,
+    message = b,
+    explanation = c,
+    verbose_explanation = d
+  ))
 }
 
 is_three_digit_code <- function(x) grepl("\\d{3}", x)
@@ -15,3 +18,7 @@ is_three_digit_code <- function(x) grepl("\\d{3}", x)
 nozero <- function(x) names(x[sapply(x, length) > 0])
 
 stopcode <- function(x, y) stop(sprintf('%s: %s\n', x, y), call. = FALSE)
+
+`%||%` <- function(x, y) if (is.null(x)) y else x
+
+comp <- function(l) Filter(Negate(is.null), l)
