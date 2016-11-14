aeea9e70721dc3274ded94fbe84551953ff733e4
Scott Chamberlain <myrmecocystus@gmail.com>
2016-11-13 19:58:46 -0800

    http to https for cat and dog status fix #4

--- R/cat_for_status.R
+++ R/cat_for_status.R
@@ -6,6 +6,8 @@
 #' @param code An http status code
 #' @param browse (logical) If \code{TRUE}, opens image in default browser. If
 #' \code{FALSE}, returns the URL of the image.
+#' @details uses a service for cats (https://http.cat) and
+#' dogs (https://httpstatusdogs.com)
 #' @return Opens image in your default browser, or returns URL
 #' @examples \dontrun{
 #' # give back url
@@ -25,7 +27,7 @@
 cat_for_status <- function(code, browse = FALSE){
   code <- as.character(code)
   if (code %in% names(status_codes)) {
-    url <- sprintf('http://httpcats.herokuapp.com/%s', code)
+    url <- sprintf('https://http.cat/%s', code)
     if (browse) utils::browseURL(url) else url
   } else {
     stopcode("No cat code found", "")
@@ -37,7 +39,7 @@ cat_for_status <- function(code, browse = FALSE){
 dog_for_status <- function(code, browse = FALSE) {
   code <- as.character(code)
   if (code %in% names(status_codes)) {
-    url <- sprintf('http://httpstatusdogs.com/wp-content/uploads/%s.jpg', code)
+    url <- sprintf('https://httpstatusdogs.com/wp-content/uploads/%s.jpg', code)
     if (browse) utils::browseURL(url) else url
   } else {
     stopcode("No dog code found", "")
