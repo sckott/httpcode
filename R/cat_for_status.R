b1e3cbf9e2f9f887a66ce6b69cb05c7c27b1224b
fubar1337 <133132398+fubar1337@users.noreply.github.com>
2023-05-10 17:39:22 +0200

    Update cat_for_status.R
    Adding goats, updating dogs URL (supports more status codes).

--- R/cat_for_status.R
+++ R/cat_for_status.R
@@ -1,4 +1,4 @@
-#' Use cat or dog pictures for various HTTP status codes
+#' Use cat or dog or goat pictures for various HTTP status codes
 #'
 #' @export
 #' @name cat_for_status
@@ -7,16 +7,19 @@
 #' @param browse (logical) If `TRUE`, opens image in default browser. If
 #' `FALSE`, returns the URL of the image.
 #' @details uses a service for cats (https://http.cat) and
-#' dogs (https://httpstatusdogs.com)
+#' dogs (https://http.dog) and goats (httpgoats.com)
 #' @return Opens image in your default browser, or returns URL
 #' @examples \dontrun{
 #' # give back url
 #' cat_for_status(100)
 #' dog_for_status(100)
+#' goat_for_status(100)
 #' cat_for_status(301)
 #' dog_for_status(301)
+#' goat_for_status(301)
 #' cat_for_status(400)
 #' dog_for_status(400)
+#' goat_for_status(400)
 #'
 #' # open image in default browser
 #' cat_for_status(400, browse=TRUE)
@@ -45,9 +48,24 @@ dog_for_status <- function(code, browse = FALSE) {
   assert(browse, "logical")
   code <- as.character(code)
   if (code %in% names(status_codes)) {
-    url <- sprintf('https://httpstatusdogs.com/wp-content/uploads/%s.jpg', code)
+    url <- sprintf('https://http.dog/%s.jpg', code)
     if (browse) utils::browseURL(url) else url
   } else {
     stopcode("No dog code found", code)
+  }
+}
+
+#' @export
+#' @rdname cat_for_status
+goat_for_status <- function(code, browse = FALSE) {
+  assert(code, c("numeric", "integer", "character"))
+  stopifnot(length(code) == 1)
+  assert(browse, "logical")
+  code <- as.character(code)
+  if (code %in% names(status_codes)) {
+    url <- sprintf('https://httpgoats.com/%s.jpg', code)
+    if (browse) utils::browseURL(url) else url
+  } else {
+    stopcode("No goat code found", code)
   }
 }
